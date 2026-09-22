22c3b40172374831331d38f5ad5137f79b8f9226
CODELOL-SYSTEM <pc2alex.les@gmail.com>
2026-09-22 16:01:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -53,8 +53,7 @@ const ADMIN_CODE = "NOVA-ADMIN-2026";
 const ADMIN_ACCESS_KEY = "novaAdminAuthorized";
 const TEST_CARD_STORAGE_KEY = "novaTestCard";
 
-const FALLBACK_IMAGE =
-  "https://placehold.co/400x300/111827/ffffff?text=NovaShop";
+
 
 // ============================================================
 // DOM
